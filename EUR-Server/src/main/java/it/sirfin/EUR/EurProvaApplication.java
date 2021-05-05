277518f24c4cc424b8ebae32db77e5a39936571c
claudiofei <78588569+claudiofei@users.noreply.github.com>
2021-05-05 10:26:34 +0200

    Creazione client e server

---
+++ EUR-Server/src/main/java/it/sirfin/EUR/EurProvaApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.EUR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurProvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurProvaApplication.class, args);
+	}
+
+}
---
+++ EUR-Server/src/test/java/it/sirfin/EUR/EurProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.EUR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurProvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
